f60b0b4fdc26cc31761ad81f3b6ed5b86694b35f
Lyew Marven <fga401@outlook.com>
2018-05-15 21:53:56 +0800

    Debug

--- CAD/src/Utility.cpp
+++ CAD/src/Utility.cpp
@@ -5,6 +5,6 @@ using namespace ACCAD;
 Vec2 ACCAD::Vec2Rotate(const Vec2 & center, const Vec2 & point, float theta)
 {
 	float tempX = point.x - center.x;
-	float tempY = point.x - center.y;
+	float tempY = point.y - center.y;
 	return Vec2(cos(theta)*tempX - sin(theta)*tempY + center.x, sin(theta)*tempX + cos(theta)*tempY + center.y);
 }
