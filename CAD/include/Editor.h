64d0d96190798c4be7b07cabbbc92f144a91d5ac
Lyew Marven <fga401@outlook.com>
2018-06-04 20:05:25 +0800

    Modify Editor

--- CAD/include/Editor.h
+++ CAD/include/Editor.h
@@ -13,45 +13,64 @@ namespace ACCAD
     class Editor
     {
     public:
-        /* Alter each pixel to given color
-         */
-        //void alterPixels(const std::vector<std::pair<Vec2, Color> > &pixels);
-
-        /* Alter all pixels to the same color
-         */
-        //void alterPixels(const std::vector<Vec2> &pixels, const Color &color);
-
         /* Start drawing. Save all drawing infomation. 
          */
         void startDraw();
-
         /* Finish drawing. Construct a Stroke instance and push it into Operation stack. 
          */
         void finishDraw();
-
         /* Set a start-point.
          */
         void setStartPoint(const Vec2i& from);
-
         /* Draw from start-point to end-point and set the next start-point as current end-point.
          */
-        void movePen(const Vec2i& to);
-        
+        void movePen(const Vec2i& to);    
         /* Set current pen.
          */
-        void setPen(const Pen& pen);
+        void setPen(const Pen*& pen);
 
-        /* Insert a figure
+        /* Insert an ellipse. Set selectedFigure as this figure and enter alternation mode.
+         * finishAlter should be called after calling this function.
          */
-        void insertFigure(IFigure *figure);
+        void insertEllipse(const Vec2i& center);
+        /* Insert an rectangle and set selectedFigure as this figure and enter alternation mode.
+         * finishAlter should be called after calling this function.
+         */
+        void insertRectangle(const Vec2i& center);
 
+        /* Start drawing a polygon.
+         */
+        void startPolygon();
+        /* Finish drawing a polygon.
+        */
+        void finishPolygon();
+        /* Add vertex to the drawing polygon.
+        */
+        void addVertex(const Vec2i& vertex);
+
+        /* Start altering a figure. In the alternation mode, you can do resize, alter vertex (only for polygon) and rotate for figure.
+         */
+        void startAlter();
+        /* Finish resizing a figure.
+        */
+        void finishAlter();
+        /* Resize an Anchor of a figure
+         */
+        void resizeFigure(IFigure *figure, int anchorId, const Vec2 &to);
         /* Alter an Anchor of a figure
-         */
-        void alterFigure(IFigure *figure, int anchorId, const Vec2 &from, const Vec2 &to);
+        */
+        void alterFigure(Polygon *figure, int anchorId, const Vec2 &to);
+        /* Rotate an Anchor of a figure
+        */
+        void rotateFigure(IFigure *figure, int anchorId, const Vec2 &to);
 
         /* Erase a figure
          */
         void eraseFigure(IFigure *figure);
+
+        /* Erase the selected figure
+         */
+        void eraseSelectedFigure();
 
     private:
         
@@ -61,7 +80,7 @@ namespace ACCAD
 
         Pen pen;
         Vec2i startPoint;
-
+        IFigure* selectedFigure;
 
         /* This is my self-designed Stack,
          * Not STL!!!
